a18c05d338ffc8ca4f966150e5e20ebac5c3bde8
Daiana tabirja <daiana.tabirja@mail.ru>
2024-06-25 10:25:52 +0300

    Add  page for each planet

--- src/api/planet.js
+++ src/api/planet.js
@@ -3,3 +3,7 @@ import {api} from "@/api/base_api.js";
 export async function getPlanets(page) {
     return api.get(`planets/?page=${page}`, {searchParams: { page:page }}).json()
 }
+
+export async function getPlanetInfo(id) {
+    return api.get(`planets/${id}`, {searchParams: {id: id}} ).json()
+}
--- src/containers/PlanetPage/PlanetPage.vue
+++ src/containers/PlanetPage/PlanetPage.vue
@@ -1,7 +1,54 @@
 <script>
+import {computed, onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
+import {getPlanetInfo} from "@/api/planet.js";
+import {getPlanetsImage} from "@/services/getPeopleData.js";
 
+export default {
+  setup() {
+
+    const planetInfo = ref('')
+    const img = ref('')
+    const route = useRoute()
+
+    const IdPlanet = computed( () => {
+      const id =  route.fullPath.replace('/planets/', '')
+      return id;
+    } )
+
+    const imgId = (id) => {
+      return getPlanetsImage(id)
+    }
+
+    onMounted( ()=> {
+      fetchPlanetInfo(parseInt(IdPlanet.value))
+    } )
+
+    async function fetchPlanetInfo(id) {
+      const response = await getPlanetInfo(id)
+      planetInfo.value = await response
+      // console.log(planetInfo.value)
+      img.value = imgId(id)
+      // console.log(img.value)
+    }
+
+    return {
+      planetInfo,
+      img
+    };
+  }
+};
 </script>
 
 <template>
+  <div>
+    <img :src="img" alt="planet photo">
 
+    <div>
+
+      <h2> {{planetInfo.name}}  </h2>
+
+    </div>
+
+  </div>
 </template>
